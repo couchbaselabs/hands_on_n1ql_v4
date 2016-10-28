270f9ae7b2aa6cf9d19db38c238c73f4b2662f96
sitaramv <sitaram.vemulapalli@couchbase.com>
2016-10-28 13:20:52 -0700

    update path for Mac cbdocloder
    Change-Id: I7510ca2383a413a2077c9ee4607a828c6faa8f26

--- load_data.sh
+++ load_data.sh
@@ -3,12 +3,24 @@
 # 
 # This script assumes Couchbase bin directory is in path
 #
+os=`uname`
+case os in
+     Linux)
+     LOADER=/opt/couchbase/bin/cbdocloader
+     ;;
+     Darwin)
+     LOADER=/Applications/Couchbase\ Server.app/Contents/Resources/couchbase-core/bin/cbdocloader
+     ;;
+     *)
+      LOADER=cbdocloader
+     ;;
+esac
 tar zxf data.tar.gz
 for b in contacts customer reviews product purchases user_profile customer_profile
 do
     echo "Populating bucket $b"
     zip -r $b $b
-    cbdocloader -u Administrator -p $1 -n 127.0.0.1:8091 -b $b -s 100 ${b}.zip
+    ${LOADER} -u Administrator -p $1 -n 127.0.0.1:8091 -b $b -s 100 ${b}.zip
     rm -rf $b
     rm -f ${b}.zip
 done
