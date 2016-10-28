0009c99e40196ba77cc6171f0751299009f1d86a
sitaramv <sitaram.vemulapalli@couchbase.com>
2016-10-28 12:44:51 -0700

    update install script to use 4.5.1
    Change-Id: I966f5f6b952ab7adf87fac5cf251bcae583a15cc

--- install_couchbase.sh
+++ install_couchbase.sh
@@ -1,8 +1,33 @@
-wget http://latestbuilds.hq.couchbase.com/couchbase-server/sherlock/$1/couchbase-server-enterprise_4.0.0-$1-ubuntu14.04_amd64.deb
-sudo /etc/init.d/couchbase-server status
-sudo dpkg -r couchbase-server
-sudo dpkg -i couchbase-server-enterprise_4.0.0-$1-ubuntu14.04_amd64.deb
-sleep 30
-/opt/couchbase/bin/couchbase-cli cluster-init --cluster-username=Administrator --cluster-password=password --cluster-ramsize=5000 --cluster-index-ramsize=1000 --services=data,index,query --cluster=localhost:8091
-sudo /etc/init.d/couchbase-server status
-rm couchbase-server-enterprise_4.0.0-$1-ubuntu14.04_amd64.deb
+cbver={$1:-2845}
+os={$2:-centos}
+osver={$3:-7} 
+downloadpath=http://172.23.120.24/builds/latestbuilds/couchbase-server/watson/${cbver}
+case os in
+     centos)
+         cbpkg=couchbase-server-enterprise-4.5.1-${cbver}-${os}${osver}.x86_64.rpm
+         wget ${downloadpath}/${cbpkg}
+         sudo /etc/init.d/couchbase-server status
+         sudo rpm -- couchbase-server
+         sudo rpm -i ${cbpkg}
+         sleep 30
+         /opt/couchbase/bin/couchbase-cli cluster-init --cluster-username=Administrator --cluster-password=password --cluster-ramsize=5000 --cluster-index-ramsize=1000 --services=data,index,query --cluster=localhost:8091
+         sudo /etc/init.d/couchbase-server status
+         rm ${cbpkg}
+         ;;
+     debin)
+     ubuntu)
+         cbpkg=couchbase-server-enterprise-4.5.1-${cbver}-${os}${osver}.amd64.deb
+         wget ${downloadpath}/${cbpkg}
+         sudo /etc/init.d/couchbase-server status
+         sudo dpkg -r couchbase-server
+         sudo dpkg -i ${cbpkg}
+         sleep 30
+         /opt/couchbase/bin/couchbase-cli cluster-init --cluster-username=Administrator --cluster-password=password --cluster-ramsize=5000 --cluster-index-ramsize=1000 --services=data,index,query --cluster=localhost:8091
+         sudo /etc/init.d/couchbase-server status
+         rm ${cbpkg}
+         ;;
+         echo "Installation is not supported"
+         exit 1
+     *)
+esac
+
