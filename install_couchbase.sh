92625b483e04af4c3417c609c697dfe21e7e0fdf
sitaramv <sitaram.vemulapalli@couchbase.com>
2016-11-01 12:17:30 -0700

    Fix issues
    Change-Id: Ib71bf05dab86758ab296f8d3aec9109e712d3abf

--- install_couchbase.sh
+++ install_couchbase.sh
@@ -1,33 +1,33 @@
-cbver={$1:-2845}
-os={$2:-centos}
-osver={$3:-7} 
+cbver=${1:-2845}
+os=${2:-centos}
+osver=${3:-7} 
 downloadpath=http://172.23.120.24/builds/latestbuilds/couchbase-server/watson/${cbver}
 case ${os} in
      centos)
          cbpkg=couchbase-server-enterprise-4.5.1-${cbver}-${os}${osver}.x86_64.rpm
          wget ${downloadpath}/${cbpkg}
          sudo /etc/init.d/couchbase-server status
-         sudo rpm -- couchbase-server
+         sudo rpm --erase couchbase-server
          sudo rpm -i ${cbpkg}
          sleep 30
-         /opt/couchbase/bin/couchbase-cli cluster-init --cluster-username=Administrator --cluster-password=password --cluster-ramsize=5000 --cluster-index-ramsize=1000 --services=data,index,query --cluster=localhost:8091
+         /opt/couchbase/bin/couchbase-cli cluster-init --cluster-username=Administrator --cluster-password=password --cluster-ramsize=2000 --cluster-index-ramsize=1000 --services=data,index,query --cluster=localhost:8091
          sudo /etc/init.d/couchbase-server status
          rm ${cbpkg}
          ;;
-     debin)
-     ubuntu)
+     debin|ubuntu)
          cbpkg=couchbase-server-enterprise-4.5.1-${cbver}-${os}${osver}.amd64.deb
          wget ${downloadpath}/${cbpkg}
          sudo /etc/init.d/couchbase-server status
          sudo dpkg -r couchbase-server
          sudo dpkg -i ${cbpkg}
          sleep 30
-         /opt/couchbase/bin/couchbase-cli cluster-init --cluster-username=Administrator --cluster-password=password --cluster-ramsize=5000 --cluster-index-ramsize=1000 --services=data,index,query --cluster=localhost:8091
+         /opt/couchbase/bin/couchbase-cli cluster-init --cluster-username=Administrator --cluster-password=password --cluster-ramsize=2000 --cluster-index-ramsize=1000 --services=data,index,query --cluster=localhost:8091
          sudo /etc/init.d/couchbase-server status
          rm ${cbpkg}
          ;;
+     *)
          echo "Installation is not supported"
          exit 1
-     *)
+         ;;
 esac
 
